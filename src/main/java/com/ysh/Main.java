e269295fd223fa65dd0d2f62aa88900921e03a77
ysh <1028509048@qq.com>
2023-10-17 10:34:41 +0800

    init project

---
+++ src/main/java/com/ysh/Main.java
@@ -0,0 +1,11 @@
+package com.ysh;
+
+/**
+ * @author shanghai.yu
+ * @date 2023/10/17 10:24
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/ysh/file/split_upload.java
@@ -0,0 +1,8 @@
+package com.ysh.file;
+
+/**
+ * @author shanghai.yu
+ * @date 2023/10/17 10:26
+ */
+public class split_upload {
+}
